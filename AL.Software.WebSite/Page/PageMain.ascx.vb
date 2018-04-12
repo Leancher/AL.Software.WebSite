f1dee8d1de8ba03fb05db15e2a8983d7d436a2b8
Leancher <Leancher@yandex.ru>
2018-04-12 21:25:05 +0300

    (no message)

--- AL.Software.WebSite/Page/PageMain.ascx.vb
+++ AL.Software.WebSite/Page/PageMain.ascx.vb
@@ -1,11 +1,44 @@
 ﻿
 Partial Class Page_MainPage
     Inherits System.Web.UI.UserControl
-
     Private Sub Page_MainPage_Load(sender As Object, e As EventArgs) Handles Me.Load
-        Dim img1 As Image = New Image()
-        img1.Visible = True
-        img1.ImageUrl = "/Pictures/Noimage.jpg"
-        Panel1.Controls.Add(img1)
+        Dim NumberCategory As Integer
+        Dim Database As New DatabaseConnect()
+        Dim Category As String
+        Database.DatabaseOpen()
+        Dim CountItem As Integer = Database.GetCountItem(Config.CategoryTable)
+        For NumberCategory = 2 To CountItem
+            Category = Database.GetItemByID(Config.CategoryTable, NumberCategory, "Name")
+            Dim TileCell As New Panel()
+            Dim PageLink As New HyperLink()
+            PageLink.NavigateUrl = Config.DefaultPage + "?category=" + Category
+            Dim TileCellPic As New Panel()
+            Dim Picture As New Image()
+            Picture.ImageUrl = GetPhotoPath(Category, NumberCategory)
+            TileCellPic.CssClass = "TileCellPic"
+            TileCellPic.Controls.Add(Picture)
+            Dim TileCellCaption As New Panel()
+            TileCellCaption.CssClass = "TileCellCaption"
+            Dim CaptionLabel As New Label()
+            CaptionLabel.Text = Database.GetItemByID(Config.CategoryTable, NumberCategory, "Caption")
+            TileCellCaption.Controls.Add(CaptionLabel)
+            TileCell.CssClass = "TileCell"
+            PageLink.Controls.Add(TileCellPic)
+            PageLink.Controls.Add(TileCellCaption)
+            TileCell.Controls.Add(PageLink)
+            TileGrid.Controls.Add(TileCell)
+        Next NumberCategory
+        Database.DatabaseClose()
     End Sub
+
+    Function GetPhotoPath(Category As String, NumberCategory As Integer) As String
+        'Получаем полный путь для проверки наличия файла
+        Dim PhotoPath As String = Config.AppPath + "Pictures\Preview\" + Category + NumberCategory.ToString + ".jpg"
+        Dim FileInfo As New System.IO.FileInfo(PhotoPath)
+        'Пока считаем, что файла не существует
+        PhotoPath = "../" + Config.PicturesFolder + "/Noimage.jpg"
+        'Если файл существует, то делаем относительный путь к файлу, полный путь не загружает картинки
+        If FileInfo.Exists = True Then PhotoPath = "../Pictures/Preview/" + Category + NumberCategory.ToString + ".jpg"
+        Return PhotoPath
+    End Function
 End Class

0d9905801ba030594c2d8ef9583cf0f481808864
leanc <leanc@DESKTOP-I4G26M3>
2018-04-19 08:53:46 +0300

    (no message)

--- AL.Software.WebSite/Page/MyNotes.ascx
+++ AL.Software.WebSite/Page/MyNotes.ascx
@@ -1,22 +1,24 @@
 ﻿<%@ Control Language="VB" AutoEventWireup="false" CodeFile="MyNotes.ascx.vb" Inherits="Page_MyNote" %>
-<div class="ContentColumn">
+<asp:Panel ID="NotesPlace" CssClass="ContentColumn" runat="server" />
+
+<%--<div class="ContentColumn">--%>
 <%
-    Dim Note As Integer = CInt(Request.QueryString("Note"))
-    If Note > 0 Then
-        Response.Write("<h3>" + NotesCaption(Note - 1) + "</h3>")
-        Response.Write(LoadSingleNote(Note))
-        Exit Sub
-    End If
-    For index = 0 To NotesPreview.Length - 1
-        Response.Write("<p>")
-        Response.Write("<h4>" + NotesCaption(index) + "</h4>")
-        Response.Write(NotesPreview(index))
-        If NotesPreview(index) <> Nothing Then
-            Response.Write("</br>")
-            Response.Write("<a href ='" + Config.DefaultPage + "?category=MyNotes&Note=" + (index + 1).ToString + "'>")
-            Response.Write("Читать дальше")
-            Response.Write("</a>")
-        End If
-    Next
+'Dim Note As Integer = CInt(Request.QueryString("Note"))
+'If Note > 0 Then
+'    Response.Write("<h3>" + NotesCaption(Note - 1) + "</h3>")
+'    Response.Write(LoadSingleNote(Note))
+'    Exit Sub
+'End If
+'For index = 0 To NotesPreview.Length - 1
+'    Response.Write("<p>")
+'    Response.Write("<h4>" + NotesCaption(index) + "</h4>")
+'    Response.Write(NotesPreview(index))
+'    If NotesPreview(index) <> Nothing Then
+'        Response.Write("</br>")
+'        Response.Write("<a href ='" + Config.DefaultPage + "?category=MyNotes&Note=" + (index + 1).ToString + "'>")
+'        Response.Write("Читать дальше")
+'        Response.Write("</a>")
+'    End If
+'Next
 %>
-</div>
+<%--</div>--%>
--- AL.Software.WebSite/Page/MyNotes.ascx.vb
+++ AL.Software.WebSite/Page/MyNotes.ascx.vb
@@ -6,38 +6,68 @@ Partial Class Page_MyNote
     Private Path As String
     Private CountItem As Integer = 0
     Private Sub Page_MyNote_Load(sender As Object, e As EventArgs) Handles Me.Load
-
         Dim Database As New DatabaseConnect()
         Database.DatabaseOpen()
         CountItem = Database.GetCountItem("MyNotes")
-        ReDim NotesPreview(CountItem)
-        ReDim NotesCaption(CountItem)
+        Dim NumberNote As Integer = CInt(Request.QueryString("Note"))
+
+        If NumberNote > 0 Then
+            Response.Write("<h3>" + NotesCaption(NumberNote - 1) + "</h3>")
+            'Response.Write(LoadSingleNote(NumberNote))
+            Exit Sub
+        End If
         For index = 1 To CountItem
-            NotesCaption(index - 1) = Database.GetItemByID("MyNotes", index, "Caption")
+            Dim Paragraph As New Label()
+            Paragraph.Text = Database.GetItemByID("MyNotes", index, "Caption")
+
+            Dim lnk As New HyperLink()
+            lnk.NavigateUrl = Config.DefaultPage + "?category=MyNotes&Note=" + index.ToString
+            lnk.Text = "Читать дальше"
+
+            Dim lt As New Literal With {
+                .Text = "<br>"
+            }
+            NotesPlace.Controls.Add(Paragraph)
+            NotesPlace.Controls.Add(lt)
+            NotesPlace.Controls.Add(lnk)
+            NotesPlace.Controls.Add(lnk)
+
+            NotesPlace.Controls.Add(lt)
+            'Response.Write("<p>")
+            'Response.Write("<h4>" + NotesCaption(index) + "</h4>")
+            'Response.Write(NotesPreview(index))
+            'If NotesPreview(index) <> Nothing Then
+            '    Response.Write("</br>")
+            '    Response.Write("<a href ='" + Config.DefaultPage + "?category=MyNotes&Note=" + (index + 1).ToString + "'>")
+            '    Response.Write("Читать дальше")
+            '    Response.Write("</a>")
+            'End If
         Next
-        LoadPreviewNotes()
+
+        'ReDim NotesPreview(CountItem)
+        'ReDim NotesCaption(CountItem)
+        'LoadPreviewNotes()
         Database.DatabaseClose()
     End Sub
-    Private Sub LoadPreviewNotes()
-        For index = 1 To CountItem
-            Path = Config.AppPath + "Content" + "\" + "MyNote" + index.ToString + ".txt"
-            Dim FileInfo As New FileInfo(Path)
-            If FileInfo.Exists = True Then
-                Using reader As New StreamReader(Path)
-                    NotesPreview(index - 1) = Left(reader.ReadToEnd(), 300)
-                End Using
-            End If
-        Next
-    End Sub
-    Public Function LoadSingleNote(Number As Integer) As String
-        Path = Config.AppPath + "Content" + "\" + "MyNote" + Number.ToString + ".txt"
-        Dim FileInfo As New FileInfo(Path)
-        If FileInfo.Exists = True Then
-            Using reader As New StreamReader(Path)
-                Return reader.ReadToEnd()
-            End Using
-        Else
-            Return "Такой заметки не существует"
-        End If
-    End Function
+    'Private Function LoadPreviewNotes(NoteNumber As Integer) As String
+    '    Path = Config.AppPath + "Content" + "\" + "MyNote" + NoteNumber.ToString + ".txt"
+    '    Dim FileInfo As New FileInfo(Path)
+    '    If FileInfo.Exists = True Then
+    '        Using reader As New StreamReader(Path)
+    '            Return Left(reader.ReadToEnd(), 300)
+    '        End Using
+    '    End If
+    '    Return ""
+    'End Function
+    'Public Function LoadSingleNote(Number As Integer) As String
+    '    Path = Config.AppPath + "Content" + "\" + "MyNote" + Number.ToString + ".txt"
+    '    Dim FileInfo As New FileInfo(Path)
+    '    If FileInfo.Exists = True Then
+    '        Using reader As New StreamReader(Path)
+    '            Return reader.ReadToEnd()
+    '        End Using
+    '    Else
+    '        Return "Такой заметки не существует"
+    '    End If
+    'End Function
 End Class

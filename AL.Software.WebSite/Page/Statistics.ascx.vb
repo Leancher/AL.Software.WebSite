95269ea3ebf3673ec548ba93abb738b7feb97761
Leancher <Leancher@yandex.ru>
2018-04-28 08:51:37 +0300

    (no message)

--- AL.Software.WebSite/Page/Statistics.ascx.vb
+++ AL.Software.WebSite/Page/Statistics.ascx.vb
@@ -5,8 +5,6 @@
         Dim CountCategory As Integer
         Dim CountItemCategory As Integer
         Dim CountView As String
-        Dim StatStr As String = ""
-        Dim Caption As String = ""
         Database.DatabaseOpen()
         CountCategory = Database.GetCountItem(Config.CategoryTable)
         For NumberCategory = 1 To CountCategory
@@ -16,14 +14,12 @@
             For NumberItem = 1 To CountItemCategory
                 CountView = Database.GetItemByID(CategoryName, NumberItem, "Viewed")
                 If CountView <> "0" Then
-                    Caption = Database.GetItemByID(CategoryName, NumberItem, "Caption")
                     Dim Str1 As New Label()
-                    Str1.Text = Caption + " - " + CountView + "</br>"
+                    Str1.Text = Database.GetItemByID(CategoryName, NumberItem, "Caption") + " - " + CountView + "</br>"
                     StatBlcok.Controls.Add(Str1)
                 End If
             Next NumberItem
         Next NumberCategory
-        If StatStr = "" Then Response.Write("Нет просмотров")
         Database.DatabaseClose()
     End Sub
 End Class
